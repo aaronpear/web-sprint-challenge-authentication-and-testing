a8233887651ccc4391ffb0425b58f28c8ef791ed
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-01-28 14:56:08 -0800

    restriction middleware written, not tested

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,20 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../secrets/index.js');
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+  if (!token) {
+    next({ status: 401, message: 'token required' });
+  } else {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        next({ status: 401, message: 'token invalid' });
+      } else {
+        req.decodedJwt = decoded;
+        next();
+      }
+    })
+  }
   /*
     IMPLEMENT
 
